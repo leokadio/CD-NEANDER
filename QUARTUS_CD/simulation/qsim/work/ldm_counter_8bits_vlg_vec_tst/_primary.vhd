5510b79bb0e5e919a1cb07f2894cca73ed48b6af
Leo Migotto <leokadiomigotto@gmail.com>
2023-08-30 21:08:44 -0300

    removendo "lixo"

--- QUARTUS_CD/simulation/qsim/work/ldm_counter_8bits_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity ldm_counter_8bits_vlg_vec_tst is
-end ldm_counter_8bits_vlg_vec_tst;
--- QUARTUS_CD/simulation/qsim/work/ldm_fsm_tp8_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity ldm_fsm_tp8_vlg_vec_tst is
-end ldm_fsm_tp8_vlg_vec_tst;
